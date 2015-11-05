32421f0157b44b981b06eb1c7006fa7626f2b30b
vdeokar <vdeokar@ncsu.edu>
2015-11-05 18:39:41 -0500

    server modified

--- src/serverSide/Server.java
+++ src/serverSide/Server.java
@@ -1,42 +1,59 @@
 package serverSide;
+
+
 import java.net.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.io.*;
 
-public class Server extends Thread  {
-	public static Socket connectionSocket;
-	
+
+public class Server implements Runnable{
+		public Socket client;
 		
-		   Server(Socket connectionSocket) {
-		      this.connectionSocket = connectionSocket;
-		   }
+		public static List<String> table = new ArrayList<>();
+				
+		public Server(Socket peer){
+			client = peer;	
+			}
+
 				
 		public void run() {
-	      try {
-	         PrintStream pstream = new PrintStream
-	         (connectionSocket.getOutputStream());
-	         for (int i = 100; i >= 0; i--) {
-	            pstream.println(i + 
-	            " bottles of beer on the wall");
-	         }
-	         pstream.close();
-	         connectionSocket.close();
-	      }
-	      catch (IOException e) {
-	         System.out.println(e);
-	      }
-	   }
-	public static void main(String[] args) throws Exception {
-		InetAddress inetAddress = InetAddress.getByName("192.168.1.1");
-		ServerSocket welcomeSocket = new ServerSocket(65423, 65535, inetAddress );
-		
-		while(true){
-			Socket connectionSocket = welcomeSocket.accept();
-			System.out.println("Connected");
-			new Server(connectionSocket).start();
+					// TODO Auto-generated method stub
+		 try {
+				 System.out.println("inside run");
+				 System.out.println("\n\nClient connected to Registration Server");
+							
+				 BufferedReader br1= new BufferedReader(new InputStreamReader(client.getInputStream()));
+				 PrintStream pw = new PrintStream(client.getOutputStream());
+				 InetAddress address = client.getInetAddress();	// get ip address of client(i.e address to which the socket is connected)
+				 System.out.println("IP address of client : " + address.toString()); // to test 
+				 int port = client.getPort();
+				 System.out.println("Port  address on which client is connected :" + port);
+					 }
+				catch(Exception e)
+					 {
+						 // yet to insert 
+					 }
+					
+				}
+				
 			
-		}
-		
-		
-	}
 
+	public static void main(String[] args)throws IOException {
+						
+					
+					
+		ServerSocket regser = new ServerSocket(65423);
+		Socket connectionSocket;
+					
+		System.out.println("Server Started!!");
+					
+		while(true)
+		{
+			connectionSocket=regser.accept(); //listening to a new client
+			Server s =new Server(connectionSocket);
+			Thread t = new Thread(s);
+			t.start();
+		}
+	}
 }
