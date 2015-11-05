79370aa0f386a75bef6de493eaca90daed384555
vdeokar <vdeokar@ncsu.edu>
2015-11-05 18:23:25 -0500

    added server

--- src/serverSide/Server.java
+++ src/serverSide/Server.java
@@ -1,8 +1,42 @@
 package serverSide;
+import java.net.*;
+import java.io.*;
 
-public class Server {
+public class Server extends Thread  {
+	public static Socket connectionSocket;
 	
-	public static void main(String... args) {
+		
+		   Server(Socket connectionSocket) {
+		      this.connectionSocket = connectionSocket;
+		   }
+				
+		public void run() {
+	      try {
+	         PrintStream pstream = new PrintStream
+	         (connectionSocket.getOutputStream());
+	         for (int i = 100; i >= 0; i--) {
+	            pstream.println(i + 
+	            " bottles of beer on the wall");
+	         }
+	         pstream.close();
+	         connectionSocket.close();
+	      }
+	      catch (IOException e) {
+	         System.out.println(e);
+	      }
+	   }
+	public static void main(String[] args) throws Exception {
+		InetAddress inetAddress = InetAddress.getByName("192.168.1.1");
+		ServerSocket welcomeSocket = new ServerSocket(65423, 65535, inetAddress );
+		
+		while(true){
+			Socket connectionSocket = welcomeSocket.accept();
+			System.out.println("Connected");
+			new Server(connectionSocket).start();
+			
+		}
+		
+		
 	}
 
 }
