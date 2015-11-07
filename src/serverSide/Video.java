fb0a57b6f0e8bcf08311a2be9848e94df4a7d03b
Aishwarya Sreenivasan <aishusreeni@gmail.com>
2015-11-07 16:37:12 -0500

    Video modified

--- src/serverSide/Video.java
+++ src/serverSide/Video.java
@@ -22,13 +22,6 @@ public class Video {
 		return other.videoID == this.videoID;
 	}
 	
-	public static void print()
-	{
-
-		//how to print the whole arraylist?
-		System.out.println(videoList.toString());
-	}
-	
 	Video(int videoID, String videoTitle, String format, int numChunks){
 		this.videoID = videoID;
 		this.videoTitle = videoTitle;
@@ -36,51 +29,41 @@ public class Video {
 		this.numChunks = numChunks;
 	}
 	
-	Video(String str,int clientID) {
-		// intro to algos:8;intro to ai:4
-		int i, j;
-		String[] val;
-		ArrayList<String> name = new ArrayList<String>();
-		String[] val_split = str.split(";");
-		int start=0;
-		int length = val_split.length;
-		val = new String[length*2];
-		for (i = 0; i < length;i++) 
-		{
-				String[]arr = val_split[i].split(":");
-				val[start++] = arr[0];
-				val[start++] = arr[1];
-				if(!videoList.contains(arr[0]))
-				{
-					this.videoID = videoIDCount++;
-					this.format="mp4";
-					this.videoTitle=arr[0];
-					this.numChunks=Integer.valueOf(arr[1]);
-					videoList.add(this);
-				}
-				VideoClient obj = new VideoClient();
-				obj.addVideoClientPair(clientID,videoID);
-				//how to print the whole arraylist?
-				print();
-				
-		}
+	public static void print()
+	{
+
+		//how to print the whole arraylist?
+		System.out.println(videoList.toString());
 	}
 	
-	void Video2(String str,int clientID) {
+	public void Video2(String str,int clientID) 
+	{
 		// Divide into params and insert into the video list and video client list
 		// intro to algos:8;intro to ai:4
 		String[] videos = str.split(";");
-		for(int vid_num=0;vid_num<videos.length;vid_num++){
+		for(int vid_num=0;vid_num<videos.length;vid_num++)
+		{
 			String[] values = videos[vid_num].split(":");
 			Video newV = new Video(-1, values[0], "mp4", Integer.parseInt(values[1]));
-			if(this.videoList.contains(newV)){
-				
+			if(!(newV.videoList.contains(newV))){
+				newV.videoID = videoIDCount++;
+				newV.format="mp4";
+				newV.videoTitle=values[0];
+				newV.numChunks=Integer.valueOf(values[1]);
+				videoList.add(newV);
 			}
-			else{
-				
-			}
+			VideoClient obj = new VideoClient();
+			obj.addVideoClientPair(clientID,videoID);
+			//how to print the whole arraylist?
+			
 		}
+		print();
 		this.videoList.toString();
+	}
+	
+	Video(String str,int clientID) 
+	{
+		Video2("intro to algos:8;intro to ai:4",3);
 	}
 	
 	public static void main(String[] args)
